653feefc3e55e0516114358865f672ba2d9dbc1a
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-19 17:15:13 +0300

    feat(pages): add main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { HashRouter, Route, Routes } from "react-router";
+
+import Layout from "./pages/layout";
+import TasksPage from "./pages/tasks-page";
+
 function App() {
-  return <></>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<TasksPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/tasks-page.tsx
@@ -0,0 +1,5 @@
+const TasksPage = () => {
+  return <>page</>;
+};
+
+export default TasksPage;
