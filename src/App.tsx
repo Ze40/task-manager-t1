a3e65f701f1ba68e12efaa90ce6468f300f94d5d
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-20 14:04:47 +0300

    fix(pages): remove add task page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { HashRouter, Route, Routes } from "react-router";
 
-import AddTaskPage from "./pages/add-task-page";
 import DonedTasksPage from "./pages/doned-tasks-page";
 import Layout from "./pages/layout";
 import TasksPage from "./pages/tasks-page";
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<TasksPage />} />
           <Route path="doned" element={<DonedTasksPage />} />
-          <Route path="add-task" element={<AddTaskPage />} />
         </Route>
       </Routes>
     </HashRouter>
